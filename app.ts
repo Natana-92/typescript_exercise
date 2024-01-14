ebc294adbab5cd80eaa064001b8e9eb1659a4315
Natana <wnsduq8282@gmail.com>
2024-01-14 22:43:31 +0900

    Add tsconfig.json to my typescript project.

--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
-console.log(hello);
+function add(
+  n1: number,
+  n2: number,
+  showResult: boolean,
+  resultParase: string
+) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(resultParase + result);
+  }
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultParase = "Result is :";
+
+add(number1, number2, true, resultParase);
